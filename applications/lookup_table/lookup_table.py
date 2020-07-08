837a4cc7344f504d488a2b1ad03d3dba2862e137
alanblee <alanbenlee12@gmail.com>
2020-07-08 15:51:46 -0700

    expensive seq test ok

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -12,13 +12,15 @@ def slowfun_too_slow(x, y):
     return v
 
 
+cache = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-    cache = {}
 
     def slowfun_inner(x, y):
         if (x, y) not in cache:
@@ -34,4 +36,4 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f"{i}: {x},{y}: {slowfun_too_slow(x, y)}")
+    print(f"{i}: {x},{y}: {slowfun(x, y)}")
